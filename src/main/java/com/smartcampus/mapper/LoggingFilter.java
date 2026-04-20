cc7e28f3e1a1c78b3425bc163fbb92427f95c92a
Pasindu Malshan <you@example.com>
2026-04-20 12:22:54 +0530

    feat(part5): implement request and response logging filter using java.util.logging

---
+++ src/main/java/com/smartcampus/mapper/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.smartcampus.mapper;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info("[REQUEST] " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("[RESPONSE] Status: " + responseContext.getStatus());
+    }
+}
